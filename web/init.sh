9d9abc8c040e09c7d05fbe5914d479d5f5bbb5d7
AlN <kilovoltov@yandex.ru>
2018-01-11 12:37:17 +0500

    clear

--- web/init.sh
+++
@@ -1,6 +0,0 @@
-mv box/web web
-rm -rf box
-sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
